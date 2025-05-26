657b3100dd87c5c02db1007f6634f4d814bfa3a1
Kaufko <99084056+Kaufko@users.noreply.github.com>
2025-05-26 12:36:38 +0200

    Fixed flake8 warnings

--- init.py
+++ init.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, render_template, jsonify, redirect
-import json
+from flask import Flask, request, render_template, redirect
 from settings import write_config, get_config
 from server_manager import load_servers, get_servers
 
@@ -10,18 +9,22 @@ def home():
     servers = get_servers()
     return render_template("index.html", servers=servers)
 
+
 @app.route('/autoload-servers', methods=['POST'])
 def autoload_servers():
     load_servers()
     return redirect('/')
+
 
 @app.route('/settings')
 def settings():
     config = get_config()
     return render_template("settings.html", default_server_path=config.get('default_server_path'))
 
+
 @app.route('/submit-setting', methods=['POST'])
 def submit_settings():
     write_config(request.form.to_dict())
     config = get_config()
-    return render_template("settings.html", default_server_path=config.get('default_server_path'))
+    return render_template("settings.html", default_server_path=config.get('default_server_path')
+    
